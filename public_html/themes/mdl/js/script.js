9906eddfef02c5930d94abd43872b328795f6187
Joe Ausdenmoore <jausdenmoore@sgcmail.com>
2018-03-18 15:36:45 -0500

    js update

--- public_html/themes/mdl/js/script.js
+++ public_html/themes/mdl/js/script.js
@@ -1,13 +1,3 @@
-"use strict";
-document.addEventListener('DOMContentLoaded', function (event) {
-    console.log('DOM fully loaded and parsed');
-    //console.log(drupalSettings);
-});
-window.addEventListener('load', function() {
-    console.log('window and all contents loaded');
-    //console.log(drupalSettings);
-});
-
 (function ($) {
 
   Drupal.behaviors.jpatrick =  {
